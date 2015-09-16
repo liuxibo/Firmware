4d3a259542dc9213900f8cd94b1d0434fc9612be
Mark Charlebois <charlebm@gmail.com>
2015-09-16 15:30:21 -0700

    Merge branch 'poll_test' into staging_develop

--- unittests/run_tests.sh
+++ unittests/run_tests.sh
@@ -4,3 +4,5 @@ make clean
 make all
 ./mixer_test
 ./sbus2_test ../../../../data/sbus2/sbus2_r7008SB_gps_baro_tx_off.txt
+
+# This is a TEST comment to trigger the merge request Jenkins job. Please do NOT approve.

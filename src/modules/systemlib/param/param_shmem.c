6b6eadaa1d6f789cc3b35709d9b3b26e8d05b307
Ramakrishna Kintada <rkintada@gmail.com>
2015-10-02 17:25:29 -0700

    Updates the copyright notice files
    Signed-off-by: Ramakrishna Kintada <rkintada@gmail.com>
    
     # Please enter the commit message for your changes. Lines starting

--- src/modules/systemlib/param/param_shmem.c
+++ src/modules/systemlib/param/param_shmem.c
@@ -2,7 +2,7 @@
 
 /****************************************************************************
  *
- * Copyright (c) 2015 Mark Charlebois. All rights reserved.
+ * Copyright (c) 2015 Ramakrishna Kintada. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
